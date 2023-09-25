930937febefd0fffa813c1d2deb890af86f06078
Salva <salvadornietoredondo3@gmail.com>
2023-09-25 21:23:39 +0200

    Bikram 1 terminado

---
+++ index.js
@@ -0,0 +1,76 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = 2 * PI;
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes
+let contarHasta10_2 = 0;
+
+for (let i = 0; contarHasta10_2 !== 10; i++) {
+  contarHasta10_2++;
+}
+
+console.log(contarHasta10_2); // El valor de contarHasta10_2 será 10
+
+let postl = 0;
+let postJ = 0;
+
+for (let i = 0; i < 11; i++) {
+    postI += postJ++;
+  }
+
+  let sumaPares = 0
+  
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {sumaPares += i
+
+    }
+    
+  }
+
+let variableValorNumerico = 15;
+const MiNombre = Salva;
+const MiNumeroFav = 7;
+let booleano1 = true;
+const booleano2 = false;
+booleanoOr = booleano1 || booleano2;
+
+console.log(booleanoOr);
+
+const booleanoMix1 = (booleano1 && (TAU/2 === PI)) || (variableValorNumerico >= MiNumeroFav);
+console.log(booleanoMix1)
+
+const seisNoEsNueve = (6 !== 9);
+const booleanoMix2 = variableValorNumerico > 0 || variableValorNumerico < -(MiNumeroFav*TAU);
+console.log(booleanoMix2)
+
+const valorSuma = MiNumeroFav + variableValorNumerico; 
+const valorResta = MiNumeroFav - variableValorNumerico;
+const valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+const valorDivision = MiNumeroFav / 3; 
+
+let contarHasta10 = 0
+
+while (contarHasta10 !== 10) {
+  contarHasta10++;
+}
+
+let preI = 0
+let preJ = 0
+for (let i = 0; i < 11; i++) {
+  preI += (++PreJ);
+  }
+
+  let sumaImpares = 0;
+  
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 != 0) {sumapares + i}
+    
+  }
+console.log(sumaImpares)
